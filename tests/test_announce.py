0ba8a946ef0d99ba2685468ece4f38918301892f
Giovanni Collazo <hello@gcollazo.com>
2014-02-21 19:34:05 -0400

    More emit tests

--- tests/test_announce.py
+++ tests/test_announce.py
@@ -1,4 +1,6 @@
 import unittest
+import json
+
 from announce import Announce
 
 class TestAnnounce(unittest.TestCase):
@@ -6,12 +8,16 @@ class TestAnnounce(unittest.TestCase):
         self.announce = Announce(_debug_mode=True)
 
     def test_emit(self):
-        sample = '"args": ["", "5:::{\\"name\\": \\"status\\", \\"args\\": [{\\"msg\\": \\"This is a test\\", \\"countdown\\": 1000}]}", null, []]'
+        expected = {'nodeId': '197520823', 'args': ['', '5:::{"name": "status", "args": [{"countdown": 1000, "msg": "This is a test"}]}', None, []]}
 
-        result = self.announce.emit(
-            'status', {'msg': 'This is a test', 'countdown': 1000})
+        result = json.loads(self.announce.emit(
+            'status', {'msg': 'This is a test', 'countdown': 1000}))
 
-        self.assertIn(sample, result)
+
+        self.assertEqual(expected['args'][0], result['args'][0])
+        self.assertEqual(len(expected['args'][1]), len(result['args'][1]))
+        self.assertEqual(expected['args'][1][:4], result['args'][1][:4])
+        self.assertEqual(expected.keys(), result.keys())
 
 
 if __name__ == '__main__':
